d6b279e49cd6dd345fa80cf02483c30d4264dde6
hoangtrung03 <vht03032000@gmail.com>
2023-03-22 15:46:20 +0700

    +Add: setup axios react query

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from 'src/App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      //ngan click ra ngoai va click lai vao trang thi refetch api
+      refetchOnWindowFocus: false
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </BrowserRouter>
   </React.StrictMode>
 )
