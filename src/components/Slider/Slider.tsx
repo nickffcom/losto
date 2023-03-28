ed168eabccfb3c5ea2829545fd46e45e1bd77128
vuhoangtrungbin0303 <hoangtrung@mobcec.com>
2023-03-28 11:02:51 +0700

    +Add: slider framer motion homepage

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,10 +1,25 @@
-import SwiperCore, { Pagination, Navigation, Autoplay, EffectCreative } from 'swiper'
+import { Link } from 'react-router-dom'
+import path from 'src/constants/path'
+import SwiperCore, { Navigation, Autoplay, EffectCreative } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
+import { motion } from 'framer-motion'
 import 'swiper/swiper-bundle.min.css'
+import { useCallback, useState } from 'react'
 
-SwiperCore.use([Pagination, Navigation, Autoplay])
+SwiperCore.use([Navigation, Autoplay, EffectCreative])
 
 const Slider = () => {
+  const [activeIndex, setActiveIndex] = useState(0)
+  const [swiper, setSwiper] = useState<SwiperCore | null>(null)
+
+  const handleSlideChangeStart = useCallback(() => {
+    setActiveIndex(-1)
+  }, [])
+
+  const handleSlideChangeEnd = useCallback(() => {
+    setActiveIndex(swiper?.realIndex ?? 0)
+  }, [swiper])
+
   return (
     <Swiper
       spaceBetween={0}
@@ -20,13 +35,13 @@ const Slider = () => {
         }
       }}
       autoplay={{
-        delay: 3000,
+        delay: 5000,
         disableOnInteraction: false
       }}
-      navigation
-      modules={[Navigation]}
-      loop
-      className='swiper-home'
+      modules={[Navigation, EffectCreative, Autoplay]}
+      onSlideChangeTransitionStart={handleSlideChangeStart}
+      onSlideChangeTransitionEnd={handleSlideChangeEnd}
+      onSwiper={setSwiper}
     >
       <SwiperSlide>
         <div className='relative'>
@@ -36,10 +51,30 @@ const Slider = () => {
             width='1920'
             height='940'
           />
-          <div className='absolute top-1/4 hidden max-w-screen-sm text-center xsm:translate-x-1/4 md:block'>
-            <p className='fs-30'>4K Resolution</p>
-            <h2 className='text-7xl'>Exclusive Steel Frame</h2>
-          </div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: activeIndex === 0 ? 1 : 0 }}
+            transition={{ duration: 0.8 }}
+            className='absolute top-0 left-0 bottom-0 right-0 hidden md:block'
+          >
+            <div className='flex h-full items-center justify-around'>
+              <div className='text-center'>
+                <p className='fs-30'>4K Resolution</p>
+                <h2 className='mt-2 text-7xl'>Exclusive Steel Frame</h2>
+                <Link to={path.productlist} title='Shop Now'>
+                  <motion.div
+                    initial={{ opacity: 0, y: -20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.5, delay: 0.6 }}
+                    className='button-primary mt-5 inline-block'
+                  >
+                    Shop Now
+                  </motion.div>
+                </Link>
+              </div>
+              <div></div>
+            </div>
+          </motion.div>
         </div>
       </SwiperSlide>
       <SwiperSlide>
@@ -50,10 +85,30 @@ const Slider = () => {
             width='1920'
             height='940'
           />
-          <div className='absolute top-1/4 hidden max-w-screen-sm text-center xsm:translate-x-1/4 md:block'>
-            <p className='fs-30'>Zania Black Edition</p>
-            <h2 className='text-7xl'>Curvy Bevel Dual Audio</h2>
-          </div>
+          <motion.div
+            className='absolute top-0 left-0 bottom-0 right-0 hidden md:block'
+            initial={{ opacity: 0 }}
+            animate={{ opacity: activeIndex === 1 ? 1 : 0 }}
+            transition={{ duration: 0.8 }}
+          >
+            <div className='flex h-full items-center justify-around'>
+              <div></div>
+              <div className='text-center text-white'>
+                <p className='fs-30'>Curvy Bevel Dual Audio</p>
+                <h2 className='mt-2 text-7xl'>Zania Black Edition</h2>
+                <Link to={path.productlist} title='Shop Now'>
+                  <motion.div
+                    initial={{ opacity: 0, y: -20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.5, delay: 0.6 }}
+                    className='button-primary mt-5 inline-block'
+                  >
+                    Shop Now
+                  </motion.div>
+                </Link>
+              </div>
+            </div>
+          </motion.div>
         </div>
       </SwiperSlide>
       <SwiperSlide>
@@ -64,10 +119,30 @@ const Slider = () => {
             width='1920'
             height='940'
           />
-          <div className='absolute top-1/4 hidden max-w-screen-sm text-center xsm:translate-x-1/4 md:block'>
-            <p className='fs-30'>Delta Zertiga Processor</p>
-            <h2 className='text-7xl'>Full Screen Display</h2>
-          </div>
+          <motion.div
+            className='absolute top-0 left-0 bottom-0 right-0 hidden md:block'
+            initial={{ opacity: 0 }}
+            animate={{ opacity: activeIndex === 2 ? 1 : 0 }}
+            transition={{ duration: 0.8 }}
+          >
+            <div className='flex h-full items-center justify-around'>
+              <div></div>
+              <div className='text-center text-white'>
+                <p className='fs-30'>Delta Zertiga Processor</p>
+                <h2 className='mt-2 text-7xl'>Full Screen Display</h2>
+                <Link to={path.productlist} title='Shop Now'>
+                  <motion.div
+                    initial={{ opacity: 0, y: -20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.5, delay: 0.6 }}
+                    className='button-primary mt-5 inline-block'
+                  >
+                    Shop Now
+                  </motion.div>
+                </Link>
+              </div>
+            </div>
+          </motion.div>
         </div>
       </SwiperSlide>
     </Swiper>
