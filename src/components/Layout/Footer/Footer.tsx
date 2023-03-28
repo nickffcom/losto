5f7063252f6465993f0b3fb6859a0e9a79a2a67f
vuhoangtrungbin0303 <hoangtrung@mobcec.com>
2023-03-28 13:59:08 +0700

    +Fix: small

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -4,13 +4,13 @@ import logo from 'src/assets/logo.svg'
 export default function Footer() {
   return (
     <footer className='mt-20 py-5 font-lotso-ecom'>
-      <div className='container'>
-        <div className='mb-2 flex items-center justify-center lg:mb-4'>
+      <div className='max-w-screen-xl container mx-auto flex flex-col items-center justify-center px-4'>
+        <div className='mb-2 lg:mb-0'>
           <Link to='/'>
-            <img src={logo} alt='logo' width='166' height='51' />
+            <img src={logo} alt='logo' width={166} height={51} />
           </Link>
         </div>
-        <p className='text-primary-1A162E text-center font-semibold dark:text-white lg:text-lg'>
+        <p className='text-primary-1A162E mt-4 text-center font-semibold dark:text-white lg:text-lg'>
           ©2023 - Trung. All rights reserved.
         </p>
       </div>
