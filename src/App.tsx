983a0eeeadccc87799d2e4509ec1eed74dccdecc
vuhoangtrungbin0303 <hoangtrung@mobcec.com>
2023-03-31 15:22:34 +0700

    +Add: scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import useRouteElements from './useRouteElements'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import ScrollTop from './components/ScrollTop'
 
 function App() {
   const routeElements = useRouteElements()
   return (
     <div>
+      <ScrollTop />
       {routeElements} <ToastContainer />
     </div>
   )
---
+++ src/components/ScrollTop/ScrollTop.tsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react'
+
+const ScrollTop = () => {
+  const [isVisible, setIsVisible] = useState(false)
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 300) {
+        setIsVisible(true)
+      } else {
+        setIsVisible(false)
+      }
+    }
+
+    window.addEventListener('scroll', toggleVisibility)
+
+    return () => window.removeEventListener('scroll', toggleVisibility)
+  }, [])
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
+
+  return (
+    <div className='b-sd-1 fixed bottom-5 right-5 z-50 rounded-full bg-gray-100 duration-200 hover:bg-secondary-26B374 hover:text-white'>
+      {isVisible && (
+        <button onClick={scrollToTop} className='p-3'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='h-6 w-6'
+          >
+            <path strokeLinecap='round' strokeLinejoin='round' d='M4.5 15.75l7.5-7.5 7.5 7.5' />
+          </svg>
+        </button>
+      )}
+    </div>
+  )
+}
+
+export default ScrollTop
